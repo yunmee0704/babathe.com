2ca8a9a2ac3292130527678c9da800efdfff2f17
yunmee0704 <900704630c@gmail.com>
2022-02-08 01:16:48 +0900

    슬라이드 추가

--- js/main.js
+++ js/main.js
@@ -8,3 +8,20 @@ $(function () {
  })
 
 })
+
+
+let swiper = new Swiper(".mySwiper", {
+   slidesPerView: 3,
+   spaceBetween: 30,
+   
+   pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".btn_next",
+      prevEl: ".btn_prev",
+    },
+  });
+
+
