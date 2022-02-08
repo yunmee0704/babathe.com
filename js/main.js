86e2db4815b2f3ff8c2b68fe731f709a830df95c
yunmee0704 <900704630c@gmail.com>
2022-02-08 22:28:10 +0900

    슬라이드수정

--- js/main.js
+++ js/main.js
@@ -1,27 +1,30 @@
 $(function () {
- $('.gnb .title a').click(function(e){
+  $('.gnb .title a').click(function (e) {
     e.preventDefault();
     $(this).addClass('on').siblings().removeClass('on')
     target = $(this).attr('href');
     $(target).addClass('on').siblings().removeClass('on')
-  
- })
+
+  })
 
 })
 
 
+
+
+
 let swiper = new Swiper(".mySwiper", {
-   slidesPerView: 3,
-   spaceBetween: 30,
-   
-   pagination: {
-      el: ".swiper-pagination",
-      type: "progressbar",
-    },
-    navigation: {
-      nextEl: ".btn_next",
-      prevEl: ".btn_prev",
-    },
-  });
+  slidesPerView: 3,
+  spaceBetween: 30,
+  grabCursor: true,
+  centeredSlides: true,
 
-
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".btn_next",
+    prevEl: ".btn_prev",
+  },
+});
